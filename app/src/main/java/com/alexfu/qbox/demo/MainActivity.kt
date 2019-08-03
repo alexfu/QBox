cac79dfb8e9a2fd3c648a90936a3f7eae0837959
Alex Fu <alex.fu914@gmail.com>
2019-08-03 13:37:31 -0400

    Implement basic view finder.

--- app/src/main/java/com/alexfu/qbox/demo/MainActivity.kt
+++ app/src/main/java/com/alexfu/qbox/demo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.alexfu.qbox.demo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.alexfu.qbox.QBoxCameraActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.scanButton).setOnClickListener {
+            startActivity(Intent(this, QBoxCameraActivity::class.java))
+        }
     }
 }
--- library/src/main/java/com/alexfu/qbox/QBoxCameraActivity.kt
+++ library/src/main/java/com/alexfu/qbox/QBoxCameraActivity.kt
@@ -1,11 +1,38 @@
 package com.alexfu.qbox
 
 import android.os.Bundle
+import android.util.Rational
+import android.util.Size
+import android.view.TextureView
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 
 class QBoxCameraActivity : AppCompatActivity() {
+    private val viewFinder by lazy<TextureView> { findViewById(R.id.com_alexfu_qbox__view_finder) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.com_alexfu_qbox__activity_camera)
+
+        val previewConfig = PreviewConfig.Builder()
+            .setTargetAspectRatio(Rational(1, 1))
+            .setTargetResolution(Size(640, 640))
+            .setLensFacing(CameraX.LensFacing.BACK)
+            .build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener(::updateViewFinder)
+
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun updateViewFinder(output: Preview.PreviewOutput) {
+        val parent = viewFinder.parent as ViewGroup
+        parent.removeView(viewFinder)
+        parent.addView(viewFinder)
+        viewFinder.surfaceTexture = output.surfaceTexture
     }
 }
