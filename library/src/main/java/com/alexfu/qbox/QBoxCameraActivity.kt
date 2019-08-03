e7a03078de946fb83d7b3211ea220a7e5b98e9bc
Alex Fu <alex.fu914@gmail.com>
2019-08-03 13:10:46 -0400

    Add library module.

---
+++ library/src/main/java/com/alexfu/qbox/QBoxCameraActivity.kt
@@ -0,0 +1,11 @@
+package com.alexfu.qbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QBoxCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.com_alexfu_qbox__activity_camera)
+    }
+}
