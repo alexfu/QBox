bda461875bdf6751725830196eb4791f43df0d94
Alex Fu <alex.fu914@gmail.com>
2019-08-03 13:44:49 -0400

    Add QR analyzer placeholder.

--- library/src/main/java/com/alexfu/qbox/QBoxCameraActivity.kt
+++ library/src/main/java/com/alexfu/qbox/QBoxCameraActivity.kt
@@ -1,14 +1,14 @@
 package com.alexfu.qbox
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Rational
 import android.util.Size
 import android.view.TextureView
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 
 class QBoxCameraActivity : AppCompatActivity() {
     private val viewFinder by lazy<TextureView> { findViewById(R.id.com_alexfu_qbox__view_finder) }
@@ -17,6 +17,7 @@ class QBoxCameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.com_alexfu_qbox__activity_camera)
 
+        // Configure preview use case
         val previewConfig = PreviewConfig.Builder()
             .setTargetAspectRatio(Rational(1, 1))
             .setTargetResolution(Size(640, 640))
@@ -26,7 +27,22 @@ class QBoxCameraActivity : AppCompatActivity() {
         val preview = Preview(previewConfig)
         preview.setOnPreviewOutputUpdateListener(::updateViewFinder)
 
-        CameraX.bindToLifecycle(this, preview)
+        // Configure analysis use case
+        val analyzerConfig = ImageAnalysisConfig.Builder()
+            .setCallbackHandler(createAnalyzerHandler())
+            .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+            .build()
+
+        val analysis = ImageAnalysis(analyzerConfig)
+        analysis.analyzer = QRAnalyzer()
+
+        CameraX.bindToLifecycle(this, preview, analysis)
+    }
+
+    private fun createAnalyzerHandler(): Handler {
+        val thread = HandlerThread("com.alexfu.qbox.QRAnalyzer")
+        thread.start()
+        return Handler(thread.looper)
     }
 
     private fun updateViewFinder(output: Preview.PreviewOutput) {
@@ -34,5 +50,11 @@ class QBoxCameraActivity : AppCompatActivity() {
         parent.removeView(viewFinder)
         parent.addView(viewFinder)
         viewFinder.surfaceTexture = output.surfaceTexture
+    }
+
+    private class QRAnalyzer : ImageAnalysis.Analyzer {
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+            // TODO: Run image through ML Kit
+        }
     }
 }
